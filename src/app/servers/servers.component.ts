c41c8c28d6ecdb58ec93eeeae42d8a08cf208d7f
Zviad Mikeladze <zviadipf@gmail.com>
2022-11-29 18:22:14 +0400

    two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent implements OnInit {
+  alloNewServer = false;
+  serverName = 'testserver';
+  serverCreationStatus = 'No server was created!';
+  constructor() {
+    setTimeout(() => {
+      this.alloNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {}
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created!';
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
